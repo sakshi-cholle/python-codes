ff0c38f24e27d43d0d089c569370001528ff3d0e
sakshi cholle <sakshicholle05@gmail.com>
2025-07-26 16:10:02 +0530

    Update P-7 oops concept using classes.py

--- P-7 oops concept using classes.py
+++ P-7 oops concept using classes.py
@@ -14,6 +14,50 @@ p = Person() #creating object
 p.showinfo()
 
 #INHERITANCE
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        return f"{self.name} makes a sound"
+
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+dog = Dog("Buddy")
+print(dog.speak())
+print(cat.speak())
+
+
+#OVERRIDING
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+a = Animal()
+d = Dog()
+a.speak()
+d.speak()
+
+
+#POLYMORPHISM
+class Cat:
+    def speak(self):
+        return "Meow"
+
+class Dog:
+    def speak(self):
+        return "Woof"
+def animal_sound(animal):
+    print(animal.speak())
+
+cat = Cat()
+dog = Dog()
+animal_sound(cat)  
+animal_sound(dog)
+
 
 
 
